13c27b07f1ccd46d4dcb58e591e6870968d6aa01
DHUMKEKAR <NDH00887@Nmcorp.Nissan.Biz>
2023-01-02 17:38:02 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfessorsComponent } from './professors/professors.component';
+import { ProfessorListComponent } from './professors/professor-list/professor-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import {ProfessorService } from './shared/professor.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfessorsComponent,
+    ProfessorListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ProfessorService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
